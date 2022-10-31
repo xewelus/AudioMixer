31407297416071a778bd71d9a481a2396b57a4ee
dinar <dinar.mobile@gmail.com>
2022-10-31 17:44:27 +0300

    sdk style

--- AudioMixer/App.xaml.cs
+++ AudioMixer/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
+using Common;
+using CommonWinForms;
+using CommonWpf.Classes;
+using CommonWpf.Classes.UI;
 
 namespace AudioMixer
 {
@@ -9,6 +15,31 @@ namespace AudioMixer
 	{
 		public App()
 		{
+			if (Misc.IsAlreadyStarted("0A2E00D5-8D96-467B-8E0F-2B032EA916EA"))
+			{
+				return;
+			}
+
+			AppInitializer.Initialize();
+			CommonWpf.UIHelper.InRuntime = true;
+
+			ExcHandler.OnError = OnError;
+			ExceptionHandler.OnError = OnError;
+
+			try
+			{
+				Settings.Load();
+			}
+			catch (Exception ex)
+			{
+				ExcHandler.Catch(ex);
+			}
+		}
+
+		private static void OnError(Exception exception)
+		{
+			string path = FS.GetAppPath("errors.log");
+			File.AppendAllText(path, $"[{DateTime.Now:dd.MM.yyyy HH:mm:ss.fff}] {exception}\r\n\r\n");
 		}
 	}
 }
