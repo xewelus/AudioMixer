97b60f8a443efe7c6a4df78fea3f1ecf64028a7a
dinar <dinar.mobile@gmail.com>
2022-10-31 15:47:20 +0300

    WpfMainPanel

---
+++ AudioMixer/WpfMainPanel.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Common;
+using CommonWpf;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace AudioMixer
+{
+	public partial class WpfMainPanel
+	{
+		public WpfMainPanel()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
