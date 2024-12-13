9051c35f3a7e637a331ca0e8c0703be95b160c7e
dinar <dinar.mobile@gmail.com>
2024-12-13 03:38:05 +0300

    MixesListPanel: regenerated

--- AudioMixer/MixesListPanel.Designer.cs
+++ AudioMixer/MixesListPanel.Designer.cs
@@ -28,183 +28,172 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.lvMixes = new System.Windows.Forms.ListView();
-			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.imageList = new System.Windows.Forms.ImageList(this.components);
-			this.tsMixes = new System.Windows.Forms.ToolStrip();
-			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-			this.cmList = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-			this.miNew = new System.Windows.Forms.ToolStripMenuItem();
-			this.miCopy = new System.Windows.Forms.ToolStripMenuItem();
-			this.miDelete = new System.Windows.Forms.ToolStripMenuItem();
-			this.miPlay = new System.Windows.Forms.ToolStripMenuItem();
-			this.miPause = new System.Windows.Forms.ToolStripMenuItem();
-			this.btnMixDelete = new System.Windows.Forms.ToolStripButton();
-			this.btnMixAdd = new System.Windows.Forms.ToolStripButton();
-			this.btnDock = new System.Windows.Forms.ToolStripButton();
-			this.tsMixes.SuspendLayout();
-			this.cmList.SuspendLayout();
-			this.SuspendLayout();
+			components = new System.ComponentModel.Container();
+			lvMixes = new ListView();
+			columnHeader1 = new ColumnHeader();
+			cmList = new ContextMenuStrip(components);
+			miNew = new ToolStripMenuItem();
+			miCopy = new ToolStripMenuItem();
+			miDelete = new ToolStripMenuItem();
+			toolStripSeparator2 = new ToolStripSeparator();
+			miPlay = new ToolStripMenuItem();
+			miPause = new ToolStripMenuItem();
+			imageList = new ImageList(components);
+			tsMixes = new ToolStrip();
+			btnMixDelete = new ToolStripButton();
+			btnMixAdd = new ToolStripButton();
+			toolStripSeparator1 = new ToolStripSeparator();
+			btnDock = new ToolStripButton();
+			cmList.SuspendLayout();
+			tsMixes.SuspendLayout();
+			SuspendLayout();
 			// 
 			// lvMixes
 			// 
-			this.lvMixes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-			this.lvMixes.ContextMenuStrip = this.cmList;
-			this.lvMixes.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.lvMixes.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.lvMixes.FullRowSelect = true;
-			this.lvMixes.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-			this.lvMixes.HideSelection = false;
-			this.lvMixes.Location = new System.Drawing.Point(0, 25);
-			this.lvMixes.MultiSelect = false;
-			this.lvMixes.Name = "lvMixes";
-			this.lvMixes.Size = new System.Drawing.Size(259, 313);
-			this.lvMixes.SmallImageList = this.imageList;
-			this.lvMixes.Sorting = System.Windows.Forms.SortOrder.Ascending;
-			this.lvMixes.TabIndex = 3;
-			this.lvMixes.UseCompatibleStateImageBehavior = false;
-			this.lvMixes.View = System.Windows.Forms.View.Details;
-			this.lvMixes.ItemActivate += new System.EventHandler(this.lvMixes_ItemActivate);
-			this.lvMixes.SelectedIndexChanged += new System.EventHandler(this.lvMixes_SelectedIndexChanged);
-			this.lvMixes.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lvMixes_KeyDown);
-			this.lvMixes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lvMixes_MouseClick);
-			this.lvMixes.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lvMixes_MouseMove);
+			lvMixes.Columns.AddRange(new ColumnHeader[] { columnHeader1 });
+			lvMixes.ContextMenuStrip = cmList;
+			lvMixes.Dock = DockStyle.Fill;
+			lvMixes.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+			lvMixes.FullRowSelect = true;
+			lvMixes.HeaderStyle = ColumnHeaderStyle.None;
+			lvMixes.Location = new Point(0, 25);
+			lvMixes.Margin = new Padding(4, 3, 4, 3);
+			lvMixes.MultiSelect = false;
+			lvMixes.Name = "lvMixes";
+			lvMixes.Size = new Size(302, 365);
+			lvMixes.SmallImageList = imageList;
+			lvMixes.Sorting = SortOrder.Ascending;
+			lvMixes.TabIndex = 3;
+			lvMixes.UseCompatibleStateImageBehavior = false;
+			lvMixes.View = View.Details;
+			lvMixes.ItemActivate += lvMixes_ItemActivate;
+			lvMixes.SelectedIndexChanged += lvMixes_SelectedIndexChanged;
+			lvMixes.KeyDown += lvMixes_KeyDown;
+			lvMixes.MouseClick += lvMixes_MouseClick;
+			lvMixes.MouseMove += lvMixes_MouseMove;
 			// 
 			// columnHeader1
 			// 
-			this.columnHeader1.Width = 100;
-			// 
-			// imageList
-			// 
-			this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-			this.imageList.ImageSize = new System.Drawing.Size(16, 16);
-			this.imageList.TransparentColor = System.Drawing.Color.Transparent;
-			// 
-			// tsMixes
-			// 
-			this.tsMixes.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
-			this.tsMixes.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnMixDelete,
-            this.btnMixAdd,
-            this.toolStripSeparator1,
-            this.btnDock});
-			this.tsMixes.Location = new System.Drawing.Point(0, 0);
-			this.tsMixes.Name = "tsMixes";
-			this.tsMixes.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-			this.tsMixes.Size = new System.Drawing.Size(259, 25);
-			this.tsMixes.TabIndex = 2;
-			// 
-			// toolStripSeparator1
-			// 
-			this.toolStripSeparator1.Name = "toolStripSeparator1";
-			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+			columnHeader1.Width = 100;
 			// 
 			// cmList
 			// 
-			this.cmList.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.miNew,
-            this.miCopy,
-            this.miDelete,
-            this.toolStripSeparator2,
-            this.miPlay,
-            this.miPause});
-			this.cmList.Name = "cmList";
-			this.cmList.Size = new System.Drawing.Size(158, 120);
-			this.cmList.Opening += new System.ComponentModel.CancelEventHandler(this.cmList_Opening);
-			// 
-			// toolStripSeparator2
-			// 
-			this.toolStripSeparator2.Name = "toolStripSeparator2";
-			this.toolStripSeparator2.Size = new System.Drawing.Size(154, 6);
+			cmList.Items.AddRange(new ToolStripItem[] { miNew, miCopy, miDelete, toolStripSeparator2, miPlay, miPause });
+			cmList.Name = "cmList";
+			cmList.Size = new Size(125, 120);
+			cmList.Opening += cmList_Opening;
 			// 
 			// miNew
 			// 
-			this.miNew.Image = global::AudioMixer.Properties.Resources.plus;
-			this.miNew.Name = "miNew";
-			this.miNew.Size = new System.Drawing.Size(157, 22);
-			this.miNew.Text = "New";
-			this.miNew.Click += new System.EventHandler(this.miNew_Click);
+			miNew.Image = Properties.Resources.plus;
+			miNew.Name = "miNew";
+			miNew.Size = new Size(124, 22);
+			miNew.Text = "New";
+			miNew.Click += miNew_Click;
 			// 
 			// miCopy
 			// 
-			this.miCopy.Image = global::AudioMixer.Properties.Resources.copy;
-			this.miCopy.Name = "miCopy";
-			this.miCopy.Size = new System.Drawing.Size(157, 22);
-			this.miCopy.Text = "Duplicate";
-			this.miCopy.Click += new System.EventHandler(this.miCopy_Click);
+			miCopy.Image = Properties.Resources.copy;
+			miCopy.Name = "miCopy";
+			miCopy.Size = new Size(124, 22);
+			miCopy.Text = "Duplicate";
+			miCopy.Click += miCopy_Click;
 			// 
 			// miDelete
 			// 
-			this.miDelete.Image = global::AudioMixer.Properties.Resources.close;
-			this.miDelete.Name = "miDelete";
-			this.miDelete.Size = new System.Drawing.Size(157, 22);
-			this.miDelete.Text = "Delete";
-			this.miDelete.Click += new System.EventHandler(this.miDelete_Click);
+			miDelete.Image = Properties.Resources.close;
+			miDelete.Name = "miDelete";
+			miDelete.Size = new Size(124, 22);
+			miDelete.Text = "Delete";
+			miDelete.Click += miDelete_Click;
+			// 
+			// toolStripSeparator2
+			// 
+			toolStripSeparator2.Name = "toolStripSeparator2";
+			toolStripSeparator2.Size = new Size(121, 6);
 			// 
 			// miPlay
 			// 
-			this.miPlay.Image = global::AudioMixer.Properties.Resources.play;
-			this.miPlay.Name = "miPlay";
-			this.miPlay.Size = new System.Drawing.Size(157, 22);
-			this.miPlay.Text = "Play";
-			this.miPlay.Click += new System.EventHandler(this.miPlay_Click);
+			miPlay.Image = Properties.Resources.play;
+			miPlay.Name = "miPlay";
+			miPlay.Size = new Size(124, 22);
+			miPlay.Text = "Play";
+			miPlay.Click += miPlay_Click;
 			// 
 			// miPause
 			// 
-			this.miPause.Image = global::AudioMixer.Properties.Resources.pause2;
-			this.miPause.Name = "miPause";
-			this.miPause.Size = new System.Drawing.Size(157, 22);
-			this.miPause.Text = "Stop";
-			this.miPause.Click += new System.EventHandler(this.miPause_Click);
+			miPause.Image = Properties.Resources.pause2;
+			miPause.Name = "miPause";
+			miPause.Size = new Size(124, 22);
+			miPause.Text = "Stop";
+			miPause.Click += miPause_Click;
+			// 
+			// imageList
+			// 
+			imageList.ColorDepth = ColorDepth.Depth8Bit;
+			imageList.ImageSize = new Size(16, 16);
+			imageList.TransparentColor = Color.Transparent;
+			// 
+			// tsMixes
+			// 
+			tsMixes.GripStyle = ToolStripGripStyle.Hidden;
+			tsMixes.Items.AddRange(new ToolStripItem[] { btnMixDelete, btnMixAdd, toolStripSeparator1, btnDock });
+			tsMixes.Location = new Point(0, 0);
+			tsMixes.Name = "tsMixes";
+			tsMixes.RightToLeft = RightToLeft.Yes;
+			tsMixes.Size = new Size(302, 25);
+			tsMixes.TabIndex = 2;
 			// 
 			// btnMixDelete
 			// 
-			this.btnMixDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.btnMixDelete.Enabled = false;
-			this.btnMixDelete.Image = global::AudioMixer.Properties.Resources.minus;
-			this.btnMixDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnMixDelete.Name = "btnMixDelete";
-			this.btnMixDelete.Size = new System.Drawing.Size(23, 22);
-			this.btnMixDelete.Text = "Delete";
-			this.btnMixDelete.Click += new System.EventHandler(this.btnMixDelete_Click);
+			btnMixDelete.DisplayStyle = ToolStripItemDisplayStyle.Image;
+			btnMixDelete.Enabled = false;
+			btnMixDelete.Image = Properties.Resources.minus;
+			btnMixDelete.ImageTransparentColor = Color.Magenta;
+			btnMixDelete.Name = "btnMixDelete";
+			btnMixDelete.Size = new Size(23, 22);
+			btnMixDelete.Text = "Delete";
+			btnMixDelete.Click += btnMixDelete_Click;
 			// 
 			// btnMixAdd
 			// 
-			this.btnMixAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.btnMixAdd.Image = global::AudioMixer.Properties.Resources.plus;
-			this.btnMixAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnMixAdd.Name = "btnMixAdd";
-			this.btnMixAdd.Size = new System.Drawing.Size(23, 22);
-			this.btnMixAdd.Text = "New";
-			this.btnMixAdd.Click += new System.EventHandler(this.btnMixAdd_Click);
+			btnMixAdd.DisplayStyle = ToolStripItemDisplayStyle.Image;
+			btnMixAdd.Image = Properties.Resources.plus;
+			btnMixAdd.ImageTransparentColor = Color.Magenta;
+			btnMixAdd.Name = "btnMixAdd";
+			btnMixAdd.Size = new Size(23, 22);
+			btnMixAdd.Text = "New";
+			btnMixAdd.Click += btnMixAdd_Click;
+			// 
+			// toolStripSeparator1
+			// 
+			toolStripSeparator1.Name = "toolStripSeparator1";
+			toolStripSeparator1.Size = new Size(6, 25);
 			// 
 			// btnDock
 			// 
-			this.btnDock.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.btnDock.Image = global::AudioMixer.Properties.Resources.dock_top;
-			this.btnDock.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.btnDock.Name = "btnDock";
-			this.btnDock.Size = new System.Drawing.Size(23, 22);
-			this.btnDock.Text = "Change window position";
-			this.btnDock.Click += new System.EventHandler(this.btnDock_Click);
+			btnDock.DisplayStyle = ToolStripItemDisplayStyle.Image;
+			btnDock.Image = Properties.Resources.dock_top;
+			btnDock.ImageTransparentColor = Color.Magenta;
+			btnDock.Name = "btnDock";
+			btnDock.Size = new Size(23, 22);
+			btnDock.Text = "Change window position";
+			btnDock.Click += btnDock_Click;
 			// 
 			// MixesListPanel
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.lvMixes);
-			this.Controls.Add(this.tsMixes);
-			this.Name = "MixesListPanel";
-			this.Size = new System.Drawing.Size(259, 338);
-			this.tsMixes.ResumeLayout(false);
-			this.tsMixes.PerformLayout();
-			this.cmList.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
-
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			Controls.Add(lvMixes);
+			Controls.Add(tsMixes);
+			Margin = new Padding(4, 3, 4, 3);
+			Name = "MixesListPanel";
+			Size = new Size(302, 390);
+			cmList.ResumeLayout(false);
+			tsMixes.ResumeLayout(false);
+			tsMixes.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
